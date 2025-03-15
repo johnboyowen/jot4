50a1eec95434fa0a023804ad7d2991558933acc1
Dikshant Rajput <dikshantraj2001@gmail.com>
2025-03-15 19:45:17 +0530

    sw version updated

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_SITE_SIGN_IN = 'site-sign-in-cache-v51';
-const CACHE_DEER_CULL = 'deer-cull-cache-v51';
-const CACHE_OBSERVATIONS = 'observations-cache-v51';
-const GENERAL_CACHE = 'general-cache-v51';
+const CACHE_SITE_SIGN_IN = 'site-sign-in-cache-v52';
+const CACHE_DEER_CULL = 'deer-cull-cache-v52';
+const CACHE_OBSERVATIONS = 'observations-cache-v52';
+const GENERAL_CACHE = 'general-cache-v52';
 
 // Install event
 self.addEventListener('install', (event) => {
