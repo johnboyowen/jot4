827794cce175807c412cc25ef0d859c570d18e66
johnboyowen <john.owen11@yahoo.co.uk>
2025-04-07 11:59:59 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_SITE_SIGN_IN = 'site-sign-in-cache-v59';
-const CACHE_DEER_CULL = 'deer-cull-cache-v59';
-const CACHE_OBSERVATIONS = 'observations-cache-v59';
-const GENERAL_CACHE = 'general-cache-v59';
+const CACHE_SITE_SIGN_IN = 'site-sign-in-cache-v60';
+const CACHE_DEER_CULL = 'deer-cull-cache-v60';
+const CACHE_OBSERVATIONS = 'observations-cache-v60';
+const GENERAL_CACHE = 'general-cache-v60';
 
 // Install event
 self.addEventListener('install', (event) => {
